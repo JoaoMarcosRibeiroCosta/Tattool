ca2ea076b14819f4edb3283ac41f263171716670
Samuel Ribeiro <samcos@alunos.utfpr.edu.br>
2025-06-14 09:51:19 -0300

    carregar tabela

--- src/main/java/Controller/ProdutoController.java
+++ src/main/java/Controller/ProdutoController.java
@@ -9,8 +9,10 @@ import Controller.ConexaoSQLServer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import javax.swing.table.DefaultTableModel;
 
-public class ProdutoController extends ConexaoSQLServer {
+public class ProdutoController  extends ConexaoSQLServer{
 
     public boolean inserirProduto(Produto produto) {
         String sql = "INSERT INTO Produto (descricao, valor, quantidade) VALUES (?, ?, ?)";
@@ -65,8 +67,9 @@ public class ProdutoController extends ConexaoSQLServer {
 
     public boolean excluirProduto(int id) {
         String sql = "DELETE FROM Produto WHERE id = ?";
-        try (Connection con = conectar(); 
-             PreparedStatement stmt = con.prepareStatement(sql)) {
+        try (
+            Connection con = conectar();     
+            PreparedStatement stmt = con.prepareStatement(sql)) {
             stmt.setInt(1, id);
             stmt.executeUpdate();
             return true;
@@ -74,6 +77,42 @@ public class ProdutoController extends ConexaoSQLServer {
             System.out.println("Erro ao excluir produto: " + e.getMessage());
             return false;
         }
+    }
+    
+    
+    public DefaultTableModel carregarTabela(String filtro) {
+        DefaultTableModel model = new DefaultTableModel();
+        String sql = "SELECT * FROM Produto"+filtro;
+        try (
+            Connection con = conectar();     
+            PreparedStatement stmt = con.prepareStatement(sql)) {
+             
+            ResultSet rs = stmt.executeQuery(sql);
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int colunas = rsmd.getColumnCount();
+
+            for (int i = 1; i <= colunas; i++) {
+                model.addColumn(rsmd.getColumnName(i));
+            }
+
+            while (rs.next()) {
+                Object[] linha = new Object[colunas];
+                for (int i = 1; i <= colunas; i++) {
+                    linha[i - 1] = rs.getObject(i);
+                }
+                model.addRow(linha);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar dados: " + e.getMessage());
+        }
+
+        return model;
     }
 
 }
